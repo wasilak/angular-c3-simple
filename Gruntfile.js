beb64209b48652f073500f3b58008e5c17fa2fd0
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2014-11-23 20:18:58 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        sourceMap: false,
+        compress: {
+          drop_console: true
+        }
+      },
+      js: {
+        files: {
+          'dist/angular_c3_simple.min.js': 'src/angular_c3_simple.js'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+};
---
+++ src/angular_c3_simple.js
@@ -0,0 +1,55 @@
+// using C3 (be sure to include it before, same with D3, which C3 requires)
+;(function(c3) {
+    'use strict';
+
+    // module definition, this has to be included in your app
+    angular.module('angular-c3-simple', [])
+
+    // service definition, if you want to use itm you have to include it in controller
+    // this service allows you to access every chart by it's ID and thanks to this,
+    // you can perform any API call available in C3.js http://c3js.org/examples.html#api
+    .service('c3SimpleService', function() {
+        return {};
+    })
+
+    // directive definition, if you want to use itm you have to include it in controller
+    .directive('c3Simple', ['c3SimpleService', function(c3SimpleService) {
+        return {
+          // this directive can be used as an Element or an Attribute
+          restrict: 'EA',
+          scope: {
+            // setting config attribute to isolated scope
+            // config object is 1:1 configuration C3.js object, for avaiable options see: http://c3js.org/examples.html
+            config: '='
+          },
+          template: '<div></div>',
+          replace: true,
+          link: function(scope, element, attrs) {
+
+            // binding chart to element with provided ID
+            scope.config.bindto = '#' + attrs.id;
+
+            //Generating the chart on every data change
+            scope.$watch('config.data.columns', function(newSeries, oldSeries) {
+
+              // if there is no data for columns, we're setting this, in order to show nothing
+              // instead of empty line chart
+              if (scope.config.data.columns.length === 0) {
+                  scope.config.data.columns = ['no data', 0];
+              }
+
+              // adding (or overwriting) chart to service c3SimpleService
+              // we are regenerating chart on each change - this might seem slow and unefficient
+              // but works pretty well and allows us to have more controll
+              c3SimpleService[scope.config.bindto] = c3.generate(scope.config);
+
+              // if there is no size specified, we are assuming, that chart will have width
+              // of its container (proportional of course) - great for responsive design
+              if (!scope.config.size) {
+                  c3SimpleService[scope.config.bindto].resize();
+              }
+            });
+          }
+        };
+    }]);
+}(c3));
